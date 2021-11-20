c923f884f6172b4266e5077b3f41f82ef97eaa4d
kelvin mjuni <bigirwak@gmail.com>
2021-11-20 13:33:02 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const httpm = require('@actions/http-client')
+const btoa = require("btoa");
+
+const beem_url = "https://apisms.beem.africa/v1/send"
+try {
+    const destAddr = core.getInput("dest_addr");
+    const message = core.getInput("message")
+    const apiKey = core.getInput("api-key")
+    const apiSecret = core.getInput("api-scret")
+    let _httpm = new httpm.HttpClient()
+    core.debug(`Sending message: ${message}.to ${destAddr}`);
+    _httpm.postJson(
+      beem_url,
+      {
+        source_addr: source_addr,
+        schedule_time: "",
+        encoding: 0,
+        message: message,
+        recipients: [
+          {
+            recipient_id: 1,
+            dest_addr: destAddr,
+          },
+        ],
+      },
+      {
+        "Content-Type": content_type,
+        Authorization: "Basic " + btoa(apiKey + ":" + apiSecret),
+      }
+    ).then((res) =>{
+        core.setOutput("status", res.code);
+        if(res.statusCode == 200){
+            core.debug("Message was successfull sent!")
+            core.debug(res.result);
+            core.setOutput("success", res.result.message);
+        }else{
+            core.setOutput("error", res.result.message);
+            core.debug(`Got an error while sending message: ${res.result}`);
+        }
+    }).catch((err) => {
+        core.setFailed(err)
+    });
+    
+
+} catch (error) {
+     core.setFailed(err);
+}
